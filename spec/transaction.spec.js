086241fb142a970de601e0829f7b70829084c556
ndowkunda <mn057@outlook.com>
2021-04-07 00:31:37 +0100

    edit tests

--- spec/transaction.spec.js
+++ spec/transaction.spec.js
@@ -1,16 +1,21 @@
-const Transaction = require('./transaction.js')
+const Transaction = require('../src/transaction.js')
 
 describe('Transaction tests', function () {
 
-it('Expecting an amount to be deposited ', function () {
-let deposit = new Transaction('deposit', 1000)
-result = deposit.activity
+it('Expecting a deposit transaction', function () {
+let deposit1 = new Transaction('14/01/2012','deposit', 1000)
+result = deposit1.activity
 expect(result).toEqual('deposit')
 });
   
 it('Expecting an amount to be deposited ', function () {
-let deposit = new Transaction('deposit', 1000)
-result = deposit.amount
+let deposit2 = new Transaction('14/01/2012','deposit', 1000)
+result = deposit2.amount
 expect(result).toEqual(1000)
+});
+  it('Expecting an amount to be deposited on a certain date', function () {
+let deposit3 = new Transaction('14/01/2012','deposit', 1000)
+result = deposit3.date
+expect(result).toEqual('14/01/2012')
 });
 })
