f06e2e5b9633ef84798ddd40bee70568bf92d140
ndowkunda <mn057@outlook.com>
2021-04-06 09:13:37 +0100

    src altered not complete

--- src/bank.js
+++ src/bank.js
@@ -1,8 +1,26 @@
+const Transaction = require('./src/transaction.js')  
 class Bank {
-  // generate statement with headings
-  print(...statement) {
-    let title = 'date || credit || debit || balance'
-    return title +'\n'+ statement
+  constructor(balance = 0) {
+    this.balance = balance
+    this.transactions = []
+  } 
+ 
+  deposit(date, transaction) {
+    (this.balance += money).toFixed(2)
+    transaction = new Transaction(activity, amount)
+    if (this.activity == 'deposit') {
+      this.transactions.push(`${date} || ${this.activity} || || ${(this.balance += this.amount).toFixed(2)}`)
+    }
   }
-}
+     withdraw(date, transaction) {
+        transaction = new Transaction(activity, amount)
+        if (this.activity == 'withdraw') {
+          this.transactions.push(`${date} || ${this.activity} || || ${(this.balance += this.amount).toFixed(2)}`)
+    }
+  }
+      getTransactions() {
+        return this.transactions
+    }
+  }
+
 module.exports = Bank
