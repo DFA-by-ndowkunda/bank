113dd14341bc66c83ab9e8e44d4846652cff10e7
ndowkunda <mn057@outlook.com>
2021-04-07 18:49:34 +0100

    add os module

--- spec/statement.spec.js
+++ spec/statement.spec.js
@@ -1,9 +1,10 @@
+const os = require('os');
 const Statement = require('../src/statement.js');
 const Transaction = require('../src/transaction.js');
 const Account = require('../src/Account.js');
 
 describe('Feature test', function () {
-  it('Expecting print() to display tranactions in a statement', function () {
+  fit('Expecting print() to display tranactions in a statement', function () {
     //setup
     let account = new Account(0)
     let deposit1 = new Transaction('10/01/2012', 'deposit', 1000)
@@ -16,6 +17,6 @@ describe('Feature test', function () {
     let statement = new Statement(account.transactions)
     let result = statement.print()
     //verify
-    expect(result).toEqual('date || credit || debit || balance\n14/01/2012 ||  || 500.00 || 2500.00\n13/01/2012 || 2000.00 || || 3000.00\n10/01/2012 || 1000.00 || || 1000.00 ');
+    expect(result).toEqual(`date || credit || debit || balance${os.EOL}14/01/2012 || || 500.00 || 2500.00${os.EOL}13/01/2012 || 2000.00 || || 3000.00${os.EOL}10/01/2012 || 1000.00 || || 1000.00 `);
 });
 });
