e42af18b18856b7e7a26a31c61b6a87ab3a13bf5
ndowkunda <mn057@outlook.com>
2021-04-07 18:56:27 +0100

    tests passing

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -6,13 +6,13 @@ describe('Account tests', function () {
     let transaction = new Transaction('14/01/2012','deposit',1000)
     let account = new Account(0)
     account.storeTransaction(transaction)
-    expect(account.transactions).toEqual(['14/01/2012 || 1000.00 || || 1000.00 '])
+    expect(account.transactions).toEqual(['14/01/2012 || 1000.00 || || 1000.00'])
 })
     it('Expecting account to store transaction', function () {
     let transaction = new Transaction('14/01/2012','withdraw',1000)
       let account = new Account(1000)
     account.storeTransaction(transaction)
-    expect(account.transactions).toEqual(['14/01/2012 || || 1000.00 || 0.00 '])
+    expect(account.transactions).toEqual(['14/01/2012 || || 1000.00 || 0.00'])
     })
       it('Expecting account to store multiple transactions', function () {
         let transaction1 = new Transaction('14/01/2012', 'withdraw', 1000)
@@ -20,8 +20,8 @@ describe('Account tests', function () {
       let account = new Account(2000)
         account.storeTransaction(transaction1)
         account.storeTransaction(transaction2)
-    expect(account.transactions).toEqual(['14/01/2012 || || 1000.00 || 1000.00 ',
-  '13/01/2012 || || 1000.00 || 0.00 '])
+    expect(account.transactions).toEqual(['14/01/2012 || || 1000.00 || 1000.00',
+  '13/01/2012 || || 1000.00 || 0.00'])
 })
 
 })
