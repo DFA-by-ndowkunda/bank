5a7239ada4dc53ae2d3a04b6ae35076d94c5938c
ndowkunda <mn057@outlook.com>
2021-04-07 18:50:01 +0100

    add os module

--- src/statement.js
+++ src/statement.js
@@ -1,17 +1,16 @@
+const os = require('os');
 class Statement {
   constructor(transactions) {
     this.transactions = transactions
-   
   }
   // generate statement with headings
   print() {
     let statement  = 'date || credit || debit || balance';
     for (let i = this.transactions.length -1;  i >=0;  i-- ){
-       statement+=`\n${this.transactions[i]}`
+       statement+=`${os.EOL}${this.transactions[i]}`
     }
        return statement
   }
-    
-  
+
 }
 module.exports = Statement
