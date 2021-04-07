8cfc4f42e8954650be0e161e437d30576e1d9f23
ndowkunda <mn057@outlook.com>
2021-04-07 18:57:26 +0100

    tests passing

--- src/account.js
+++ src/account.js
@@ -7,11 +7,11 @@ class Account {
   storeTransaction(transaction) {
     if (transaction.activity == 'deposit') {
       let newBalance = this.balance += transaction.amount
-      this.transactions.push(`${transaction.date} || ${transaction.amount.toFixed(2)} || || ${newBalance.toFixed(2)} `)
+      this.transactions.push(`${transaction.date} || ${transaction.amount.toFixed(2)} || || ${newBalance.toFixed(2)}`)
     }
     if (transaction.activity == 'withdraw') {
       let newBalance = this.balance -= transaction.amount
-      this.transactions.push(`${transaction.date} || || ${transaction.amount.toFixed(2)} || ${newBalance.toFixed(2)} `)
+      this.transactions.push(`${transaction.date} || || ${transaction.amount.toFixed(2)} || ${newBalance.toFixed(2)}`)
     }
     return this.transactions
   }
