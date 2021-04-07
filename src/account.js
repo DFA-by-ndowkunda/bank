5780e20c73d635f00ae5adb300a467fd50e6b0d6
ndowkunda <mn057@outlook.com>
2021-04-07 18:50:49 +0100

    add os module

---
+++ src/account.js
@@ -0,0 +1,19 @@
+class Account {
+  constructor(balance = 0) {
+    this.balance = balance
+    this.transactions = []
+  }
+
+  storeTransaction(transaction) {
+    if (transaction.activity == 'deposit') {
+      let newBalance = this.balance += transaction.amount
+      this.transactions.push(`${transaction.date} || ${transaction.amount.toFixed(2)} || || ${newBalance.toFixed(2)} `)
+    }
+    if (transaction.activity == 'withdraw') {
+      let newBalance = this.balance -= transaction.amount
+      this.transactions.push(`${transaction.date} || || ${transaction.amount.toFixed(2)} || ${newBalance.toFixed(2)} `)
+    }
+    return this.transactions
+  }
+}
+module.exports = Account
